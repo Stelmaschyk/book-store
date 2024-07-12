557fdd5068a4a600663810526a96a8caa9f3fe61
svg <stelmaschyk.vg@gmail.com>
2024-07-12 15:51:11 +0300

    first commit

---
+++ src/main/java/mate/academy/bookstore/BookStoreApplication.java
@@ -0,0 +1,11 @@
+package mate.academy.bookstore;
+
+
+@SpringBootApplication
+public class BookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreApplication.class, args);
+	}
+
+}
