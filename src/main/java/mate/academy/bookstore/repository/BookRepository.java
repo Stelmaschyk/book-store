36cf3c287fb0f9b6040be9b5c6a181141bcff617
svg <stelmaschyk.vg@gmail.com>
2024-07-30 19:41:23 +0300

    task about liquidbase

--- src/main/java/mate/academy/bookstore/repository/BookRepository.java
+++ src/main/java/mate/academy/bookstore/repository/BookRepository.java
@@ -1,13 +1,11 @@
 package mate.academy.bookstore.repository;
 
-import java.util.List;
 import java.util.Optional;
 import mate.academy.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface BookRepository {
-    Book save(Book book);
-
-    List<Book> findAll();
-
+public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query(value = "SELECT * FROM books WHERE id = id", nativeQuery = true)
     Optional<Book> getBookById(Long id);
 }
