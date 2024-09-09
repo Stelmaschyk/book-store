9b8de5fb93a49508d6e427c4401317a80d6bb717
svg <stelmaschyk.vg@gmail.com>
2024-09-09 17:01:52 +0300

    Pagination, Sorting, and Swagger task

--- src/main/java/mate/academy/bookstore/service/BookService.java
+++ src/main/java/mate/academy/bookstore/service/BookService.java
@@ -5,11 +5,12 @@ import mate.academy.bookstore.dto.BookDto;
 import mate.academy.bookstore.dto.BookSearchParametersDto;
 import mate.academy.bookstore.dto.CreateBookRequestDto;
 import mate.academy.bookstore.dto.UpdateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
