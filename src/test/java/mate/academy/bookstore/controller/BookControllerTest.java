6df5bb9189155e24924a7a711cd6168843010dc4
svg <stelmaschyk.vg@gmail.com>
2024-12-03 18:34:26 +0200

    add first test for BookController.class

---
+++ src/test/java/mate/academy/bookstore/controller/BookControllerTest.java
@@ -0,0 +1,72 @@
+package mate.academy.bookstore.controller;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mate.academy.bookstore.dto.book.BookDto;
+import mate.academy.bookstore.dto.book.CreateBookRequestDto;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BookControllerTest {
+    protected static MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeAll
+    static void beforeAll(@Autowired WebApplicationContext applicationContext) {
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(applicationContext)
+            .apply(springSecurity())
+            .build();
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("Create a new book")
+    void createBook_ValidRequestDto_Success() throws Exception {
+        CreateBookRequestDto requestDto = new CreateBookRequestDto()
+            .setTitle("Title")
+            .setAuthor("Author")
+            .setIsbn("978-9996989271")
+            .setPrice(BigDecimal.valueOf(22))
+            .setDescription("Boost Performance and Unleash Creativity")
+            .setCoverImage("image.jpeg")
+            .setCategoryIds(Set.of(1L));
+
+        BookDto expected = new BookDto()
+            .setTitle(requestDto.getTitle())
+            .setAuthor(requestDto.getAuthor())
+            .setIsbn(requestDto.getIsbn())
+            .setPrice(requestDto.getPrice())
+            .setDescription(requestDto.getDescription())
+            .setCoverImage(requestDto.getCoverImage())
+            .setCategoryIds(requestDto.getCategoryIds());
+
+        String jsonRequest = objectMapper.writeValueAsString(requestDto);
+
+        MvcResult result = mockMvc.perform(post("/books")
+                .content(jsonRequest)
+                .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isCreated())
+            .andReturn();
+
+        BookDto actual = objectMapper.readValue(result.getResponse().getContentAsString(), BookDto.class);
+        EqualsBuilder.reflectionEquals(expected, actual);
+    }
+}
