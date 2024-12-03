249eceb3feb3313807b974cd4e06cafc3d5695de
svg <stelmaschyk.vg@gmail.com>
2024-12-03 19:21:18 +0200

    fixed checkstyle

--- src/test/java/mate/academy/bookstore/controller/BookControllerTest.java
+++ src/test/java/mate/academy/bookstore/controller/BookControllerTest.java
@@ -1,8 +1,12 @@
 package mate.academy.bookstore.controller;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
 import java.util.Set;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import mate.academy.bookstore.dto.book.BookDto;
 import mate.academy.bookstore.dto.book.CreateBookRequestDto;
 import org.apache.commons.lang3.builder.EqualsBuilder;
@@ -17,9 +21,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BookControllerTest {
@@ -40,22 +41,22 @@ class BookControllerTest {
     @DisplayName("Create a new book")
     void createBook_ValidRequestDto_Success() throws Exception {
         CreateBookRequestDto requestDto = new CreateBookRequestDto()
-            .setTitle("Title")
-            .setAuthor("Author")
-            .setIsbn("978-9996989271")
-            .setPrice(BigDecimal.valueOf(22))
-            .setDescription("Boost Performance and Unleash Creativity")
-            .setCoverImage("image.jpeg")
-            .setCategoryIds(Set.of(1L));
+                .setTitle("Title")
+                .setAuthor("Author")
+                .setIsbn("978-9996989271")
+                .setPrice(BigDecimal.valueOf(22))
+                .setDescription("Boost Performance and Unleash Creativity")
+                .setCoverImage("image.jpeg")
+                .setCategoryIds(Set.of(1L));
 
         BookDto expected = new BookDto()
-            .setTitle(requestDto.getTitle())
-            .setAuthor(requestDto.getAuthor())
-            .setIsbn(requestDto.getIsbn())
-            .setPrice(requestDto.getPrice())
-            .setDescription(requestDto.getDescription())
-            .setCoverImage(requestDto.getCoverImage())
-            .setCategoryIds(requestDto.getCategoryIds());
+                .setTitle(requestDto.getTitle())
+                .setAuthor(requestDto.getAuthor())
+                .setIsbn(requestDto.getIsbn())
+                .setPrice(requestDto.getPrice())
+                .setDescription(requestDto.getDescription())
+                .setCoverImage(requestDto.getCoverImage())
+                .setCategoryIds(requestDto.getCategoryIds());
 
         String jsonRequest = objectMapper.writeValueAsString(requestDto);
 
@@ -63,10 +64,11 @@ class BookControllerTest {
                 .content(jsonRequest)
                 .contentType(MediaType.APPLICATION_JSON)
             )
-            .andExpect(status().isCreated())
-            .andReturn();
+                .andExpect(status().isCreated())
+                .andReturn();
 
-        BookDto actual = objectMapper.readValue(result.getResponse().getContentAsString(), BookDto.class);
+        BookDto actual = objectMapper.readValue(result.getResponse().getContentAsString(),
+                BookDto.class);
         EqualsBuilder.reflectionEquals(expected, actual);
     }
 }
