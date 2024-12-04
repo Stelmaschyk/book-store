4b41b908e1a27850c5b4130000834f1e28f6407c
svg <stelmaschyk.vg@gmail.com>
2024-12-04 15:38:17 +0200

    added test

--- src/test/java/mate/academy/bookstore/config/CustomMySqlContainer.java
+++ src/test/java/mate/academy/bookstore/config/CustomMySqlContainer.java
@@ -4,7 +4,6 @@ import org.testcontainers.containers.MySQLContainer;
 
 public class CustomMySqlContainer extends MySQLContainer<CustomMySqlContainer> {
     private static final String DB_IMAGE = "mysql:8";
-
     private static CustomMySqlContainer mysqlContainer;
 
     private CustomMySqlContainer() {
